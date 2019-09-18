36682b2e859da6b4e89c98ca2d278b55236a41ea
Miyoung Chung <mkchung.8@gmail.com>
2019-09-17 17:49:06 -0700

    used jQuery to display question and answers to the screen after pressing start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,7 +36,9 @@ $(document).ready(function(){
     ];
 
     var answersCorrect = ["G Clef", "Semibreve","Semitone","Staves","Before Time Signatures","One Semitone Higher","Divide Bars","E# G# B##","Key Signatures","88"]
-    var displayQuestion;
+    var count = 0;
+    var showQuestion; 
+    
     var answerOne;
     var answerTwo;
     var answerThree;
@@ -51,7 +53,7 @@ function startScreen() {
     $("#main-content").html("<h2><b>Are you a Music Theory Master?</b> <br> <br> Answer each question in 10 seconds.<br> Press start to begin!</h2>");
     $("#main-content").append('<img src="assets/images/startimage.gif" alt="image">');
     $("#main-content").append(startButton);
-};
+    };
 
 startScreen();
 
@@ -60,29 +62,17 @@ $("#start").on("click", function(){
    gameStart();
 });
 
+function gameStart () {
 
-function gameStart (){
+displayQuestion();
 
-    
-    
-  /* for (var i=0; i < gameQuestions.length; i++) {
-        displayQuestion = gameQuestions[i].question
-        answerOne = gameQuestions[i].answers[0]
-        answerTwo = gameQuestions[i].answers[1]
-        answerThree = gameQuestions[i].answers[2]
-        answerFour = gameQuestions[i].answers[3]
+function displayQuestion () {
+    $("#main-content").append("<h2>" + gameQuestions[count].question + "</h2>");
+    for (var i=0; i < 3; i++) {
+        $("#main-content").append("<p class='answer'>" + gameQuestions[count].answers[i] + "</p>")
+            }
+        }
+    }
 
-    
-    }; */
-    displayQuestion = gameQuestions[0].question
-    answerOne = gameQuestions[0].answers[0];
-    answerTwo = gameQuestions[0].answers[1];
-    answerThree = gameQuestions[0].answers[2];
-    answerFour = gameQuestions[0].answers[3];
-    $("#main-content").append("<h2>" + displayQuestion + "</h2>")
-    $("#main-content").append('<p class="answer">' + answerOne + '</p>')
-    $("#main-content").append('<p class="answer">' + answerTwo + '</p>')
-    $("#main-content").append('<p class="answer">' + answerThree + '</p>')
-    $("#main-content").append('<p class="answer">' + answerFour + '</p>')
-}; 
-});
+
+})
